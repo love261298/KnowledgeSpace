415f32e2eb4efdacb7864177a702316555445c5f
toandq <toandq@pmr.vn>
2025-04-22 10:23:27 +0700

    Add project files.

---
+++ src/KnowledgeSpace.BackendServer/Model/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using KnowledgeSpace.BackendServer.Model.Interfaces;
+
+public class User : IDateTracking
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
+    public DateTime CreateDate { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+
+}
